e5705ba45c0787a43b934d44f5125a05463f497c
Ira <ira350520@gmail.com>
2024-09-20 20:57:21 +0300

    Add new system out println

--- src/app/GitLesson03.java
+++ src/app/GitLesson03.java
@@ -4,6 +4,7 @@ public class GitLesson03 {
 
     public static void main(String[] args){
         System.out.println("Hello git!");
+        System.out.println("Second changes for git");
     }
 
 }
